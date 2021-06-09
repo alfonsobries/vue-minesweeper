fb6cf18157ae8b55a76bc2d9d8c39742d0c33296
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-09 13:54:46 -0500

    Board and mine

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="flex justify-center p-4 ">
     <div class="border-4 border-gray-200">
-      <div class="bg-gray-200 border-2 border-gray-100 border-t-gray-400 border-l-gray-400">
-        The board
+      <div
+        class="grid bg-gray-200 border-2 border-gray-100 border-t-gray-400 border-l-gray-400"
+        :style="`grid-template-columns: repeat(${settings.columns}, minmax(0, 1fr));grid-template-rows: repeat(${settings.rows}, minmax(0, 1fr));`"
+      >
+        <cell
+          v-for="index in [...Array(totalMines).keys()]"
+          :key="index"
+          :has-mine="mines.indexOf(index) !== -1"
+          :clicked="clickedCells.indexOf(index) !== -1"
+          :cell-index="index"
+          @clicked="onCellClicked"
+        />
       </div>
     </div>
   </div>
@@ -10,6 +20,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Cell from './Cell.vue';
 
 const presets = {
   BEGINNER: {
@@ -21,10 +32,42 @@ const presets = {
 
 export default defineComponent({
   name: 'HelloWorld',
+  components: { Cell },
   data() {
     return {
       settings: presets.BEGINNER,
+      mines: [] as Array<number>,
+      clickedCells: [] as Array<number>,
     };
+  },
+  computed: {
+    totalMines(): number {
+      return this.settings.rows * this.settings.columns;
+    },
+    maxMines(): number {
+      return this.totalMines - 1;
+    },
+  },
+  methods: {
+    initMines(initialCellIndex: number) {
+      this.mines = [];
+      const minesToAssign = [...Array(this.settings.mines)];
+      let randomCellIndex: number;
+      do {
+        randomCellIndex = Math.floor(Math.random() * this.maxMines);
+        if (this.mines.indexOf(randomCellIndex) === -1 && initialCellIndex !== randomCellIndex) {
+          minesToAssign.pop();
+          this.mines.push(randomCellIndex);
+        }
+      } while (minesToAssign.length);
+    },
+    onCellClicked(cellIndex: number) {
+      if (this.mines.length === 0) {
+        this.initMines(cellIndex);
+      }
+
+      this.clickedCells.push(cellIndex);
+    },
   },
 });
 </script>
---
+++ src/components/Cell.vue
@@ -0,0 +1,48 @@
+<template>
+  <button
+    v-if="!clicked"
+    type="button"
+    class="w-6 h-6 border-2 border-gray-100 outline-none focus:outline-none border-b-gray-400 border-r-gray-400"
+    @click="onClick"
+  />
+  <span
+    v-else
+    type="button"
+    class="flex items-center justify-center w-6 h-6 text-sm shadow-sm"
+  >
+    <template v-if="hasMine">💣</template>
+    <template v-else>{{ minesCount }}</template>
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    cellIndex: {
+      type: Number,
+      required: true,
+    },
+    hasMine: {
+      type: Boolean,
+      required: true,
+    },
+    clicked: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: ['clicked'],
+  computed: {
+    minesCount() {
+      return 1;
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit('clicked', this.cellIndex);
+    },
+  },
+});
+</script>
