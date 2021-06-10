3978bf2ac4faac0abfb81780c6b5bef08ac2e70d
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-09 20:20:10 -0500

    Typescript adjustments to the digital counter

--- src/components/DigitalCounterNumber.vue
+++ src/components/DigitalCounterNumber.vue
@@ -5,8 +5,8 @@
         <span
           class="block h-3 border-2 border-transparent  -mr-0.5"
           :class="{
-            'border-l-red-500': numbers[number][0][3],
-            'border-l-red-900 opacity-50': !numbers[number][0][3],
+            'border-l-red-500': inOn(number,0,3),
+            'border-l-red-900 opacity-50': !inOn(number,0,3),
           }"
         />
       </span>
@@ -14,16 +14,16 @@
         <span
           class="block w-3 border-2 border-transparent"
           :class="{
-            'border-t-red-500': numbers[number][0][0],
-            'border-t-red-900 opacity-50': !numbers[number][0][0],
+            'border-t-red-500': inOn(number,0,0),
+            'border-t-red-900 opacity-50': !inOn(number,0,0),
 
           }"
         />
         <span
           class="block w-3 border-2 border-transparent"
           :class="{
-            'border-b-red-500': numbers[number][0][2],
-            'border-b-red-900 opacity-50': !numbers[number][0][2],
+            'border-b-red-500': inOn(number,0,2),
+            'border-b-red-900 opacity-50': !inOn(number,0,2),
           }"
         />
       </span>
@@ -31,8 +31,8 @@
         <span
           class="block h-3 border-2 border-transparent  -ml-0.5"
           :class="{
-            'border-r-red-500': numbers[number][0][1],
-            'border-r-red-900 opacity-50': !numbers[number][0][1],
+            'border-r-red-500': inOn(number,0,1),
+            'border-r-red-900 opacity-50': !inOn(number,0,1),
           }"
         />
       </span>
@@ -42,8 +42,8 @@
         <span
           class="block h-3 border-2 border-transparent  -mr-0.5"
           :class="{
-            'border-l-red-500': numbers[number][1][3],
-            'border-l-red-900 opacity-50': !numbers[number][1][3],
+            'border-l-red-500': inOn(number,1,3),
+            'border-l-red-900 opacity-50': !inOn(number,1,3),
           }"
         />
       </span>
@@ -51,15 +51,15 @@
         <span
           class="block w-3 border-2 border-transparent"
           :class="{
-            'border-t-red-500': numbers[number][1][0],
-            'border-t-red-900 opacity-50': !numbers[number][1][0],
+            'border-t-red-500': inOn(number,1,0),
+            'border-t-red-900 opacity-50': !inOn(number,1,0),
           }"
         />
         <span
           class="block w-3 border-2 border-transparent"
           :class="{
-            'border-b-red-500': numbers[number][1][2],
-            'border-b-red-900 opacity-50': !numbers[number][1][2],
+            'border-b-red-500': inOn(number,1,2),
+            'border-b-red-900 opacity-50': !inOn(number,1,2),
           }"
         />
       </span>
@@ -67,8 +67,8 @@
         <span
           class="block h-3 border-2 border-transparent  -ml-0.5"
           :class="{
-            'border-r-red-500': numbers[number][1][1],
-            'border-r-red-900 opacity-50': !numbers[number][1][1],
+            'border-r-red-500': inOn(number,1,1),
+            'border-r-red-900 opacity-50': !inOn(number,1,1),
           }"
         />
       </span>
@@ -79,47 +79,65 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-const numbers = {
+type NumberPartConfig = [
+  [boolean, boolean, boolean, boolean],
+  [boolean, boolean, boolean, boolean],
+];
+
+type NumbersConfig = {
+  0: NumberPartConfig,
+  1: NumberPartConfig,
+  2: NumberPartConfig,
+  3: NumberPartConfig,
+  4: NumberPartConfig,
+  5: NumberPartConfig,
+  6: NumberPartConfig,
+  7: NumberPartConfig,
+  8: NumberPartConfig,
+  9: NumberPartConfig,
+
+};
+const numbers: NumbersConfig = {
   0: [
-    [1, 1, 0, 1],
-    [0, 1, 1, 1],
+    [true, true, false, true],
+    [false, true, true, true],
   ],
 
   1: [
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
+    [false, true, false, false],
+    [false, true, false, false],
   ],
   2: [
-    [1, 1, 0, 0],
-    [1, 0, 1, 1],
+    [true, true, false, false],
+    [true, false, true, true],
   ],
   3: [
-    [1, 1, 0, 0],
-    [1, 1, 1, 0],
+    [true, true, false, false],
+    [true, true, true, false],
   ],
   4: [
-    [0, 1, 0, 1],
-    [1, 1, 0, 0],
+    [false, true, false, true],
+    [true, true, false, false],
   ],
   5: [
-    [1, 0, 0, 1],
-    [1, 1, 1, 0],
+    [true, false, false, true],
+    [true, true, true, false],
   ],
   6: [
-    [1, 0, 0, 1],
-    [1, 1, 1, 1],
+    [true, false, false, true],
+    [true, true, true, true],
   ],
   7: [
-    [1, 1, 0, 0],
-    [0, 1, 0, 0],
+    [true, true, false, false],
+    [false, true, false, false],
   ],
   8: [
-    [1, 1, 0, 1],
-    [1, 1, 1, 1],
+    [true, true, false, true],
+    [true, true, true, true],
   ],
   9: [
-    [1, 1, 0, 1],
-    [1, 1, 0, 0],
+    [true, true, false, true],
+    [true, true, false, false],
   ],
 };
 
@@ -136,6 +154,10 @@ export default defineComponent({
       numbers,
     };
   },
-
+  methods: {
+    isOn(n: keyof NumbersConfig, part: 0 | 1, part2:0 | 1 | 2 | 3): boolean {
+      return numbers[n][part][part2];
+    },
+  },
 });
 </script>
